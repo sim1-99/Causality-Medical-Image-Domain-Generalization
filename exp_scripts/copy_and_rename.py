7a93407434ffe79769df77e437b95949c0abef5a
Simone CHIARELLA <simonechiarella99@gmail.com>
2024-07-16 19:11:32 +0200

    Change source folder
    Now there are two different source folders for images and labels, in order to copy them from nnUNet_raw

--- exp_scripts/copy_and_rename.py
+++ exp_scripts/copy_and_rename.py
@@ -1,29 +1,37 @@
 import os
 import shutil
 
-source_folder = '/home/schiarella/nnUNet_data/FeTA_24_FT'
-destination_folder = '/home/schiarella/nnUNet_data/FeTA_24_GIN-IPA'
+images_folder = '/home/schiarella/nnUNet_data/nnUNet_raw/Dataset005_Brains/imagesTr'
+labels_folder = '/home/schiarella/nnUNet_data/nnUNet_raw/Dataset005_Brains/labelsTr'
+destination_folder = '/home/schiarella/Causality-Medical-Image-Domain-Generalization/FeTA_24_GIN-IPA/B'
 
 # Copy all files from source folder to destination folder
-for filename in os.listdir(source_folder):
-    source_file = os.path.join(source_folder, filename)
-    if os.path.isfile(source_file):
-        shutil.copy(source_file, destination_folder)
+for filename in os.listdir(images_folder):
+    if "kcl" in filename or "SB" in filename:
+        source_file = os.path.join(images_folder, filename)
+        if os.path.isfile(source_file):
+            shutil.copy(source_file, destination_folder)
+
+for filename in os.listdir(labels_folder):
+    if "kcl" in filename or "SB" in filename:
+        source_file = os.path.join(labels_folder, filename)
+        if os.path.isfile(source_file):
+            shutil.copy(source_file, destination_folder)
 
 # Sort files in the destination folder alphabetically
 sorted_files = sorted(os.listdir(destination_folder))
 
 # Rename files based on the specified conditions
-image_count = 1
-label_count = 1
+image_count = 150
+label_count = 150
 
 for filename in sorted_files:
     file_path = os.path.join(destination_folder, filename)
-    if "_T2w" in filename:
+    if "_0000" in filename:
         new_filename = f"image_{image_count}.nii.gz"
         image_count += 1
         print(filename, " renamed as: ", new_filename)
-    elif "_label" in filename:
+    else:
         new_filename = f"label_{label_count}.nii.gz"
         label_count += 1
         print(filename, " renamed as: ", new_filename)
